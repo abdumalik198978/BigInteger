6f0810504e87f7dd4f22571dc3bf1a6788ed1ed1
abdumalikkholmirzaev <abdumalik1989@yahoo.com>
2020-02-04 17:58:02 -0600

    second commt. good evening added

--- src/tests/ForGit.java
+++ src/tests/ForGit.java
@@ -3,5 +3,6 @@ package tests;
 public class ForGit {
     public static void main(String[] args) {
         System.out.println("Hello project");
+        System.out.println("Good evening");
     }
 }
